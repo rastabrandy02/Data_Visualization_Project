832de51e545125ca8bb4714440b56f2ada0b07d7
rastabrandy02 <73582888+rastabrandy02@users.noreply.github.com>
2024-05-07 13:42:37 +0200

    Player Animations

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,19 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed;
+    public float interactRange;
+    public GameObject exclamation;
     public bool movingPlayer;
     public bool playerInMenu;
-    public bool InteractRange;
-
+    public bool isInInteractRange;
+    public Animator animator;
 
     Collider2D lastCollision;
     void Start()
     {
         movingPlayer = false;
         playerInMenu = false;
+        
     }
 
     // Update is called once per frame
@@ -38,35 +41,70 @@ public class Player : MonoBehaviour
         }
 
 
-        if (!Input.anyKey) movingPlayer = false;
+        if (!Input.anyKey)
+        {
+            movingPlayer = false;
+            
+        }
+        if (Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || 
+            Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D) && !playerInMenu)
+        {
+            movingPlayer = false;
+            animator.SetBool("isMoving", movingPlayer);
+        }
 
-       
-        if(Input.GetKeyDown(KeyCode.Space) && InteractRange)
+        if (Input.GetKeyDown(KeyCode.Space) && isInInteractRange)
         {
             Interact();
         }
-
+        if(lastCollision != null) CheckCollisionRange();
+        animator.SetBool("isMoving", movingPlayer);
+        
     }
 
     void Walk(Vector2 direction)
     {
         movingPlayer = true;
         gameObject.transform.position += new Vector3(direction.normalized.x * speed, direction.normalized.y * speed) * Time.deltaTime;
-
+        animator.SetFloat("X", direction.x);
+        animator.SetFloat("Y", direction.y);
+        animator.SetBool("isMoving", movingPlayer);
     }
     void Interact()
     {
-        if(playerInMenu)
-        {
-            movingPlayer = false;
-            playerInMenu = false;
-            lastCollision.gameObject.GetComponent<Signboard>().CloseUIMenu();
+        Signboard signboard;
+
+        if (playerInMenu)
+        {          
+            if (lastCollision.gameObject.TryGetComponent(out signboard))
+            {
+                movingPlayer = false;
+                playerInMenu = false;
+                signboard.CloseUIMenu();
+            }                
         }
         else
+        {           
+            if(lastCollision.gameObject.TryGetComponent(out signboard))
+            {
+                movingPlayer = false;
+                playerInMenu = true;
+                signboard.OpenUIMenu();
+            }       
+        }
+    }
+    void CheckCollisionRange()
+    {
+
+        if(Vector2.Distance(transform.position, lastCollision.transform.position) > interactRange)
         {
-            movingPlayer = false;
-            playerInMenu = true;
-            lastCollision.gameObject.GetComponent<Signboard>().OpenUIMenu();
+            isInInteractRange = false;
+            playerInMenu = false;
+            exclamation.SetActive(false);
+        }
+        if (Vector2.Distance(transform.position, lastCollision.transform.position) > interactRange * 3)
+        {
+            lastCollision = null;
         }
     }
     private void OnTriggerEnter2D(Collider2D collision)
@@ -74,13 +112,12 @@ public class Player : MonoBehaviour
         if (collision.gameObject.CompareTag("Interactable"))
         {
             lastCollision = collision;
-            InteractRange = true;            
+            isInInteractRange = true;
+            exclamation.SetActive(true);
         }
     }
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        InteractRange = false;
-        playerInMenu = false;
-    }
+
+
+   
 }
     
